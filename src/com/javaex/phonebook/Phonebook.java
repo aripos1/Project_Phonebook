3c0bf1170b414f418768660040467b1bea5cf78f
aripos1 <ihsjjang@gmail.com>
2024-07-17 16:56:24 +0900

    case문 완료

--- src/com/javaex/phonebook/Phonebook.java
+++ src/com/javaex/phonebook/Phonebook.java
@@ -56,7 +56,7 @@ public class Phonebook {
 					pList.get(i).showList();
 					System.out.println("");
 				}
-				break;
+				continue;
 			case 2:
 				System.out.println("<2.등록>");
 
@@ -67,7 +67,7 @@ public class Phonebook {
 				System.out.print(">휴대전화 : ");
 				String h = sc.nextLine();
 
-				System.out.print(">회사전화 : ");  
+				System.out.print(">회사전화 : ");
 				String c = sc.nextLine();
 
 				Phone p = new Phone(n, h, c);
@@ -81,11 +81,45 @@ public class Phonebook {
 					bw.newLine();
 				}
 				bw.close();
-				break;
+				continue;
 			case 3:
-			case 4:
-			case 5:
+				System.out.println("<3.삭제>");
+				System.out.print(">번호 : ");
+				int delNum = sc.nextInt();
+				int sum = delNum - 1;
+				pList.remove(sum);
+				System.out.println("[삭제되었습니다.]");
 
+				Writer out1 = new FileWriter("C:\\javaStudy\\miniProject\\PhoneDB1.txt");
+				BufferedWriter bw1 = new BufferedWriter(out1);
+				for (Phone phone : pList) {
+					bw1.write(phone.writeList());
+					bw1.newLine();
+				}
+				bw1.close();
+				continue;
+			case 4:
+				System.out.println("<4.검색>");
+				System.out.print(">이름 : ");
+				sc.nextLine();
+				String search = sc.nextLine();
+				for (int i = 0; i < pList.size(); i++) {
+					Phone p1 = pList.get(i);
+					if (p1.getName().contains(search)) {
+						int a = i + 1;
+						System.out.print(a);
+						pList.get(i).showList();
+					}
+				}
+				continue;
+			case 5:
+				System.out.println("==================");
+				System.out.println("*     감사합니다     *");
+				System.out.println("==================");
+
+				break;
+			default:
+				System.out.println("[다시 입력해주세요.]");
 			}
 
 		}
